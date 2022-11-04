5ec419b1014f890e30d451993cdfc13579189011
Bobby Luly <bobbyluly@gmail.com>
2022-11-04 12:26:56 -0600

    Add problem solving comments

--- challenge1.rb
+++ challenge1.rb
@@ -1,4 +1,8 @@
 # 1. Given an array of strings, return only the strings that have exactly 4 characters.
+# - Create an array of strings, some with exactly 4 letters, some with more than 4 letters.
+#   Code will look at each element to see which ones are exactly 4 letters and return them
+# - look at each string -- .each method.
+# - use if conditional and .length == 4 to return only the elements containing 4 letters.
 
 foods = ["pizza", "nuts", "chips", "beef", "cheese", "kiwi"]
 
