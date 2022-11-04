2e20498555c7aa1f90287348bd69bb2becaa5e7d
Bobby Luly <bobbyluly@gmail.com>
2022-11-04 12:34:17 -0600

    Add problem solving text

--- challenge2.rb
+++ challenge2.rb
@@ -1,5 +1,9 @@
 # 2. Start with an array of strings with a mix of uppercase and lowercase letters.
 # Print every word in all lowercase letters.
+# - Create an array of strings with a mix of upper case and lower case letters
+# - Code will look at each element and convert all upper case letters to lower case
+# - look at each string -- .each method.
+# - Use .downcase method to return each element in all lower case letters
 
 games = ["CoD", "Apex Legends", "Fall Guys", "Legend of Zelda"]
 
