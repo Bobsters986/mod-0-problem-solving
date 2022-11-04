6c8f1179beb8eb1d7ec025e4fff3d617a19a81a8
Bobby Luly <bobbyluly@gmail.com>
2022-11-04 12:39:14 -0600

    Add problem solving comments

--- challenge4.rb
+++ challenge4.rb
@@ -1,5 +1,9 @@
 # 4. Start with an array of strings.
 # Print only the words that include the letter combination "ing".
+# - Create an array of strings, some ending with "ing" and some not ending in "ing"
+#   Code will look at each element to see which ones end in "ing" and return them
+# - look at each string -- .each method.
+# - use if conditional and .end_with?("ing") method to return all elements ending in "ing"
 
 activities = ["running", "swimming", "sleep", "watch movie", "eating", "play golf"]
 
